af48b3bcecac50a1ffa22085ec1f7ad784f40bc6
Bejarano <44102483+vicente323@users.noreply.github.com>
2022-09-11 20:06:13 -0500

    Estructura general del proyecto terminada

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express= require('express');
 const handlebars=require('express-handlebars')
 const app= express();
 
+const path= require('path');
+
+app.use('/src',express.static(path.join(__dirname,'src')))
 app.engine('handlebars',handlebars.engine());
 app.set('view engine','handlebars');
 app.set('views','./views');
+
+
 
 const port = process.env.port|| 3000;
 
