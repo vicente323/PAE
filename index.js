4180de123e44edc74a7febad314aa76ad6855a01
Bejarano <44102483+vicente323@users.noreply.github.com>
2022-09-17 16:58:25 -0500

    bug solved

--- index.js
+++ index.js
@@ -13,9 +13,27 @@ app.engine('handlebars',handlebars.engine());
 app.set('view engine','handlebars');
 app.set('views','./views');
 
-
+var newsGlobal;
 
 const port = process.env.port|| 3000;
+
+app.get('/new',(req,res)=>{
+    var title1=req.query.title
+   
+    var fullnew;
+    newsGlobal.forEach(element => {
+        //console.log(element)
+        if(element.title==title1){
+            
+             fullnew=element
+             res.render('new',fullnew)
+        }
+       
+    });
+  
+   
+    
+})
 app.get('/search',async(req,res)=>{
     var  news;
    if(req.query.kw&&!req.query.raw){
@@ -26,7 +44,7 @@ app.get('/search',async(req,res)=>{
         .then(json=>{
         
             news=json.articles
-        
+            newsGlobal=news
         
             res.render('index',{data:news});// render {context [equivale a la respuesta de los articulos]}
             //res.send(200)
